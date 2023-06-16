08f38a660257ec86d504b58ce8b29bac7bb31126
tkdrms96 <a18rama@naver.com>
2023-06-16 16:23:57 +0900

    JPA 단방향 연관관계 매핑 one to manu

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -10,30 +10,10 @@ public class Member {
     private Long id;
     @Column(name = "USERNAME")
     private String name;
-    @Column(name = "TEAM_ID")
-    private Long teamId;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @OneToMany
+    @JoinColumn(name="team_id")
+    private Team team;
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getTeamId() {
-        return teamId;
-    }
 }
